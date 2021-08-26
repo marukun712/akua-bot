1f312f04d421211f14708e41a2771623e4be80b2
marukun_ <87289638+marukun712@users.noreply.github.com>
2021-08-26 15:13:53 +0900

    Update server.js

--- chatserver/server.js
+++ chatserver/server.js
@@ -13,37 +13,37 @@ app.get('/chat', function (req, res) {
         const response = {output: []};
         const msg = response.output;
 
-        if (userInputText == '挨拶して') {
+        if (userInputText == '挨拶') {
             msg.push({
                 type: 'text',
                 value: 'こんあくあー！湊あくあです！',
             });
-        } else if (userInputText == 'あくたんの動画が見たい') {
+        } else if (userInputText == '動画') {
             msg.push({
                 type: 'text',
                 value: 'ここから見れるよ！https://bit.ly/2Wq5sDF',
             });
-        } else if (userInputText == 'あくたんのTwitter教えて') {
+        } else if (userInputText == 'Twitter') {
             msg.push({
                 type: 'text',
                 value: 'ここから見れるよ！https://bit.ly/3gwstLY',
             });
-        } else if (userInputText == '誕生日はいつ？') {
+        } else if (userInputText == '誕生日') {
             msg.push({
                 type: 'text',
                 value: '12月1日だよ！',
             });    
-        } else if (userInputText == 'パパは誰？？') {
+        } else if (userInputText == 'パパ') {
             msg.push({
                 type: 'text',
                 value: 'がおうパッパだよ！ https://bit.ly/3sKjED6',
             });        
-        } else if (userInputText == '体重を教えて') {
+        } else if (userInputText == '体重') {
             msg.push({
                 type: 'text',
                 value: 'あああああああぁああああぃああああぁぁぁぁあぁぁぁぁぁあぁあああああぁああぁああああああぁぁぁああああぁあぁぁぁぁああぁあああぁぁぁぁぁぁぁぁぁぁぁぁぁぁぁあぁああぁぁぁぁぁあぁぁぁぁあぁぁぁぁぁぁ.......',
             });            
-        } else if (userInputText == '自己紹介して') {
+        } else if (userInputText == '自己紹介') {
             msg.push({
                 type: 'text',
                 value: '「皆さんどうもこんあくあー！湊あくあです！」マリンメイド服のバーチャルメイド。本人は頑張っているがおっちょこちょいでドジっ子。',
@@ -53,12 +53,12 @@ app.get('/chat', function (req, res) {
                 type: 'text',
                 value: 'おすすめは三周年記念グッズ！ここから買えるよ！https://hololive.booth.pm/items/3167052',
             });                  
-        } else if (userInputText == 'あくたんの曲が聞きたい') {
+        } else if (userInputText == '曲') {
             msg.push({
                 type: 'text',
                 value: '複数の音楽配信サービスで配信中だよ！https://open.spotify.com/artist/6GHr4rGFHGRUuzuCuxJA0f?autoplay=true',
             });
-        } else if (userInputText == 'こんあくあー！') {
+        } else if (userInputText == 'こんあくあ') {
             msg.push({
                 type: 'text',
                 value: 'こんあくあー！',
